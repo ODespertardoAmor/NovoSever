188e9c610852cd93dad976fc937446657dc55546
ODespertardoAmor <escritorairanelmalemos@gmail.com>
2026-05-26 09:48:32 -0300

    Create Server.js

---
+++ RotaBrasil/Server.js
@@ -0,0 +1,25 @@
+{
+  "name": "rota-brasil-backend",
+  "version": "1.0.0",
+  "description": "App de Corridas - Backend Completo",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "author": "Você ❤️",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.19.2",
+    "mongoose": "^8.4.0",
+    "cors": "^2.8.5",
+    "bcryptjs": "^2.4.3",
+    "jsonwebtoken": "^9.0.2",
+    "socket.io": "^4.7.5",
+    "dotenv": "^16.4.5",
+    "validator": "^13.12.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.0"
+  }
+}
